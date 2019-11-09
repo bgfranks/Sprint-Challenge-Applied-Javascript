f7a6135465d5871abd9a7e93b11be087fa27653c
Brandon Franks <bgfranks106@icloud.com>
2019-11-08 23:17:45 -0500

    have the card component working

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,53 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const articleParent = document.querySelector(".cards-container");
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles").then(res => {
+  const newArticle = res.data.articles;
+
+  newArticle.bootstrap.forEach(element => {
+    articleParent.append(Article(element));
+  });
+  newArticle.javascript.forEach(element => {
+    articleParent.append(Article(element));
+  });
+  newArticle.jquery.forEach(element => {
+    articleParent.append(Article(element));
+  });
+  newArticle.node.forEach(element => {
+    articleParent.append(Article(element));
+  });
+  newArticle.javascript.forEach(element => {
+    articleParent.append(Article(element));
+  });
+});
+function Article(articleContent) {
+  // Creating Elements
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const authorName = document.createElement("span");
+
+  // Adding Classes
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  // Adding Element Content
+  headline.textContent = articleContent.headline;
+  img.src = articleContent.authorPhoto;
+  authorName.textContent = `By ${articleContent.authorName}`;
+
+  // appending Children
+  card.append(headline, author);
+  author.append(imgContainer, authorName);
+  imgContainer.appendChild(img);
+
+  // return card
+  return card;
+}
