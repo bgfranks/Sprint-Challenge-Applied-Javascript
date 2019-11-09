2089fc4a65d445be0bddcadc6f31a794acba8735
Brandon Franks <bgfranks106@icloud.com>
2019-11-08 21:14:54 -0500

    finished the header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const parentHeader = document.querySelector(".header-container");
+
+function Header() {
+  // Creating DOM Elements
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const logo = document.createElement("h1");
+  const temperature = document.createElement("span");
+
+  // Adding Classes
+  header.classList.add("header");
+  date.classList.add("date");
+  temperature.classList.add("temp");
+
+  // Adding Text Content
+  date.textContent = "SMARCH 28, 2019";
+  logo.textContent = "Lambda Times";
+  temperature.textContent = "98°";
+
+  // Append Children
+  header.append(date, logo, temperature);
+
+  // Returning the header
+  return header;
+}
+
+parentHeader.appendChild(Header());
